be55bc9f3b5389bf9f1fb131a5bb1749ff872dde
Fabio9494 <fabio.casoria94@gmail.com>
2023-12-21 14:26:31 +0100

    add bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            dumbotron: 'dumbo.jpeg'
+        }
+    },
+    methods: {
+        image(path) {
+            return new URL(path, import.meta.url).href
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <div>
+        <img :src="image(`../../img/${dumbotron}`)" alt="">
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
