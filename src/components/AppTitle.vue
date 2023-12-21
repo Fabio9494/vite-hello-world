d5166fbc06267e9cffa6d3b4e54ddbe5258d5d6e
Fabio9494 <fabio.casoria94@gmail.com>
2023-12-21 13:41:08 +0100

    add title

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template lang="">
+    <div>
+       <h1>{{title}}</h1>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
